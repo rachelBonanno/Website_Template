2658c33b46d936b4a5cc5bc37e8a85a2dfe493c1
rachelbonanno <rachel.bonanno@tufts.edu>
2022-07-08 20:05:56 -0400

    Working With JavaScript

---
+++ src/app.js
@@ -0,0 +1,4 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content')
